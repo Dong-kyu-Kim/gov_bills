0dfcbdcc608e81e364a98f9a7246d332b0f08e5b
Dongkyu_Kim <154038146+Dong-kyu-Kim@users.noreply.github.com>
2024-10-08 17:42:09 -0600

    code with results

---
+++ QuickDBD-Free Diagram.sql
@@ -0,0 +1,35 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/QM63T2
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "house_rep" (
+    "name" VARCHAR(30)   NOT NULL,
+    "term_type" VARCHAR(15)   NOT NULL,
+    "party" VARCHAR(30)   NOT NULL,
+    "start_date" VARCHAR(30)   NOT NULL,
+    "end_date" VARCHAR(30)   NOT NULL,
+    "state" VARCHAR(15)   NOT NULL,
+    "district" VARCHAR(30)   NOT NULL,
+    "office_address" VARCHAR   NOT NULL,
+    "phone" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_house_rep" PRIMARY KEY (
+        "name"
+     )
+);
+
+CREATE TABLE "house_vote_tallies" (
+    "unnamed" INT   NOT NULL,
+    "bill_number" VARCHAR(15)   NOT NULL,
+    "representative" VARCHAR(100)   NOT NULL,
+    "party" VARCHAR(30)   NOT NULL,
+    "state" VARCHAR(30)   NOT NULL,
+    "vote" VARCHAR(10)   NOT NULL,
+    "link" VARCHAR(255)   NOT NULL,
+    "full_name" VARCHAR(100)   NOT NULL,
+    "phone_number" VARCHAR(30)   NOT NULL
+);
+
+ALTER TABLE "house_vote_tallies" ADD CONSTRAINT "fk_house_vote_tallies_full_name" FOREIGN KEY("full_name")
+REFERENCES "house_rep" ("name");
+
---
+++ gov_bills_sql.sql
@@ -0,0 +1,33 @@
+CREATE TABLE house_rep (
+    name VARCHAR(100),
+    term_type VARCHAR(15),
+    party VARCHAR(30),
+    start_date VARCHAR(30),
+    end_date VARCHAR(30),
+    state VARCHAR(15),
+    district varchar(30),
+    office_address VARCHAR(255),
+    phone VARCHAR(30),
+	PRIMARY KEY (name)
+);
+
+CREATE TABLE house_vote_tallies (
+	unnamed INT,
+    bill_number VARCHAR(15),
+    representative VARCHAR(100),
+    party VARCHAR(30),
+    state VARCHAR(30),
+    vote VARCHAR(10),
+    link VARCHAR(255),
+	full_name VARCHAR(100),
+	phone_number VARCHAR(30),
+	foreign key (full_name) references house_rep(name)
+);
+
+select * from house_rep;
+
+select * from house_vote_tallies;
+
+select * from house_rep
+join house_vote_tallies
+on house_rep.name = house_vote_tallies.full_name;
